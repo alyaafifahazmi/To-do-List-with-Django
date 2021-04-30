4c0decb50ebed123ab1849029e2e116f0a7519c9
Alya Afifah <77565117+alyaafifahazmi@users.noreply.github.com>
2021-05-01 04:35:51 +0800

    main

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('update_task/<str:pk>/', views.updateTask, name = "update_task"),
     path('delete/<str:pk>/', views.deleteTask, name = "delete"),
 
+    path('register/', views.registerPage, name = 'register'),
+    path('login/', views.loginPage, name = 'login'),
+    path('logout', views.logoutUser, name = 'logout'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,55 @@ from django.http import HttpResponse
 from .models import *
 from .forms import *
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('list')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
 
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'tasks/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('list')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('list')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'tasks/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+@login_required(login_url='login')
 def index(request):
     tasks = Task.objects.all()
 
@@ -20,6 +66,7 @@ def index(request):
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
 
+@login_required(login_url='login')
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
 
@@ -35,6 +82,7 @@ def updateTask(request, pk):
 
     return render(request, 'tasks/update_task.html', context)
 
+@login_required(login_url='login')
 def deleteTask(request, pk):
 
     task = Task.objects.get(id = pk)
